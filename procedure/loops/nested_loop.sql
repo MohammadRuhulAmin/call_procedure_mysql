356609a4b4c53d657d45302def7da1f185b8e18c
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-11-30 14:41:58 +0600

    nested loop added

---
+++ procedure/loops/nested_loop.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+CREATE PROCEDURE nested_loop(IN cnt INT)
+BEGIN
+DECLARE i INT DEFAULT 0;
+DECLARE j INT DEFAULT 0;
+DECLARE res VARCHAR (255) DEFAULT "";
+
+
+WHILE i <= cnt DO
+  SET res = CONCAT(res ,"x");
+  WHILE j <= 5 DO
+  SET res = CONCAT(res, "y");
+  SET j = j+1;
+  END WHILE;
+  SET j = 0;  
+  SET i = i+1;
+END WHILE; 
+
+SELECT res;
+END //
+DECLARE;
+
+CALL nested_loop(5);
